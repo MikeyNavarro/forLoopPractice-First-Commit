2fdb5a26dfe9a471913a7388c6fa9cbeaa3382f4
Mikey Navarro <mikey.navarro.135@gmail.com>
2022-07-02 15:53:25 -0700

    Python forLoop practice

---
+++ for_loop_basic1.py
@@ -0,0 +1,36 @@
+# for i in range(151):
+#   print(i)
+
+# for i in range(5,1001):
+#   if i % 5 == 0:
+#     print(i)
+
+
+# for i in range(1,101):
+#   if i % 10  == 0:
+#     print('Coding dojo')
+#   elif i % 5 == 0:
+#     print('Coding Dojo')
+#   else: print(i)
+
+
+
+
+# sum = 0
+# for i in range(500000):
+#   if i % 2 == 1:
+#     sum += i  
+
+# print(sum)
+
+# for i in range(2018, 0,-4):
+#   if i % 2 == 0:
+#     print(i)
+
+# lownum = 2
+# highnum = 9
+# mult = 3
+
+# for i in range (lownum, highnum +1, 1):
+#   if i % mult == 0:
+#     print(i)
